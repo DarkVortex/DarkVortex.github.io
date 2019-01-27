73577835818b8e90250772ec09996b8e75087945
DarkVortex <arnav98@gmail.com>
2019-01-27 17:43:23 +0000

    added temp updating on website

--- js/iotTemp.js
+++ js/iotTemp.js
@@ -2,6 +2,5 @@ let dbRef = firebase.database().ref();
 let tempRef = dbRef.child("iot");
 tempRef.on("value", function(snapshot){
   let data = snapshot.val();
-  console.log(data.temperature);
-  //document.getElementById(temp).innerText = 
+  document.getElementById(temp).innerText = data.temperature;
 });
