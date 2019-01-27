b1a9b72bfcfcd2a48ec9e1944da25a41597a6593
DarkVortex <arnav98@gmail.com>
2019-01-27 17:44:46 +0000

    fixed element name

--- js/iotTemp.js
+++ js/iotTemp.js
@@ -2,5 +2,5 @@ let dbRef = firebase.database().ref();
 let tempRef = dbRef.child("iot");
 tempRef.on("value", function(snapshot){
   let data = snapshot.val();
-  document.getElementById(temp).innerText = data.temperature;
+  document.getElementById('temp').innerText = data.temperature;
 });
