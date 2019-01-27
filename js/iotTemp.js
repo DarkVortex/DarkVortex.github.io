19ae6ee56f8b602870658de5343e5a83a8fe1259
DarkVortex <arnav98@gmail.com>
2019-01-27 17:40:23 +0000

    debug temperature logging

--- js/iotTemp.js
+++ js/iotTemp.js
@@ -2,5 +2,6 @@ let dbRef = firebase.database().ref();
 let tempRef = dbRef.child("iot");
 tempRef.on("value", function(snapshot){
   let data = snapshot.val();
-  console.log(data);
+  console.log(data.temperature);
+  //document.getElementById(temp).innerText = 
 });
