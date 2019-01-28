6d7525cf9ede90cac20015cbdbaf4bf50d65f931
DarkVortex <arnav98@gmail.com>
2019-01-28 10:33:03 +0000

    updated to add degrees F

--- js/iotTemp.js
+++ js/iotTemp.js
@@ -3,4 +3,5 @@ let tempRef = dbRef.child("iot");
 tempRef.on("value", function(snapshot){
   let data = snapshot.val();
   document.getElementById('temp').innerText = data.temperature;
+  document.getElementById('tempF').innerText = (data.temperature * 9 / 5 + 32);
 });
