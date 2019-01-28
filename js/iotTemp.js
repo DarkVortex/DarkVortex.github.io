1faaa54a201d532dd22c8aca8eacb4414a7400f4
DarkVortex <arnav98@gmail.com>
2019-01-28 10:36:41 +0000

    changed math to only get 1 decimal

--- js/iotTemp.js
+++ js/iotTemp.js
@@ -3,5 +3,7 @@ let tempRef = dbRef.child("iot");
 tempRef.on("value", function(snapshot){
   let data = snapshot.val();
   document.getElementById('temp').innerText = data.temperature;
-  document.getElementById('tempF').innerText = (data.temperature * 9 / 5 + 32);
+  degF = (data.temperature * 9 / 5 + 32);
+  deF = Math.round(degF * 10) / 10;
+  document.getElementById('tempF').innerText = degF;
 });
